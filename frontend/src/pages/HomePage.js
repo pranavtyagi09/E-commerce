f34f855c59dc129a5c5f7ec5a42b31bed7bae429
Pranav Tyagi <pranav.tyagi@cgic.cooperators.ca>
2020-07-31 16:17:04 -0400

    Adding express as backend and loading data.

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import data from '../data'
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+//import ProductPage from './ProductPage';
 
 function HomePage(props) {
+
+  const [products, setProducts] = useState([]);
+  
+  useEffect(()=>{
+    const fetchData = async () => {
+      const {data} = await axios.get("/api/products");
+      setProducts(data);
+    }
+
+    fetchData();
+    return () => {
+      //
+    };
+  }, [])
+
     return (<ul className="products">
     {
-      data.products.map(product =>
-      <li>
+      products.map(product =>
+      <li key={product._id}>
           <div className="product">
               <img className="product-image" src={product.image} alt="product" />
                 <Link to={'/product/' + product._id}>
